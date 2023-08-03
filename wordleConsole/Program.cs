2cc56e05f6c7de973e955c149f2e6865f5af2908
Anthony Meade <antnymeade@gmail.com>
2023-08-03 13:02:00 +0100

    Add project files.

---
+++ wordleConsole/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wordleConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            wordSubject subject = new wordSubject();
+
+            Console.WriteLine(subject.letterArray().ToString());
+            ;
+        }
+    }
+}
---
+++ wordleConsole/wordSubject.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wordleConsole
+{
+    internal class wordSubject
+    {
+
+
+        private string[] wordsArray = new string[] { "Test!" };
+
+        private string[] chosenArray;
+
+        public string roundWord() { return wordsArray[0]; }
+        
+        public string[] letterArray() 
+        {
+            chosenArray = wordsArray[0].Split();
+            return chosenArray;
+        }
+
+    }
+}
